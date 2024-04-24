3108ad3ac322c5271a0863a139de6fb69a988eed
Stalyin <stalyin16ac@gmail.com>
2024-04-24 07:36:42 -0500

    Movie-Web2

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,3 +20,15 @@ const searchBarIsActive = () => navbarForm.classList.toggle("active");
 
 navbarSearchBtn.addEventListener("click", searchBarIsActive);
 navbarFormClosebtn.addEventListener("click", searchBarIsActive);
+
+function openVideo(videoSrc) {
+  var autoplayUrl =
+    videoSrc + (videoSrc.includes("?") ? "&autoplay=1" : "?autoplay=1");
+  var leftPosition = window.screen.width / 2 - 1000 / 2;
+  var topPosition = window.screen.height / 2 - 600 / 2;
+  window.open(
+    autoplayUrl,
+    "_blank",
+    `width=1000,height=600,left=${leftPosition},top=${topPosition}`
+  );
+}
